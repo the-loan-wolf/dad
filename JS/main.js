c6af43cb3134362bbc82803e28598251e5c5bea5
The-loan-wolf <rishavb518@gmail.com>
2020-11-05 12:42:24 +0530

    section 1 almost complete

--- JS/main.js
+++ JS/main.js
@@ -16,3 +16,19 @@ document.querySelector(".close-icon").onclick = function() {
     document.querySelector(".nav").style.display = "none";
 
 } 
+
+//calculating available height to apply on background image
+
+let innerHeight = window.innerHeight;
+let headerHeight = document.querySelector('.header').offsetHeight;
+let imageHeight = innerHeight - headerHeight;
+document.querySelector('.section-1').style.height = imageHeight + 'px';
+
+function windowResize() {
+let innerHeight = window.innerHeight;
+let headerHeight = document.querySelector('.header').offsetHeight;
+let imageHeight = innerHeight - headerHeight;
+document.querySelector('.section-1').style.height = imageHeight + 'px';
+}
+
+
